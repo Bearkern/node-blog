62d68966412fd74263ddbe2dedb0154c24ba4fb4
Clara Cai <clara.cai@tpisoftware.com>
2024-04-08 16:14:54 +0800

    create and delete category

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -5,13 +5,21 @@ const firebaseAdminDb = require('../connections/firebase-admin');
 const categoriesRef = firebaseAdminDb.ref('/categories');
 
 router.get('/archives', (req, res, next) => {
+
   res.render('dashboard/archives', { title: 'Express' });
 });
+
 router.get('/article', (req, res, next) => {
   res.render('dashboard/article', { title: 'Express' });
 });
+
 router.get('/categories', (req, res, next) => {
-  res.render('dashboard/categories', { title: 'Express' });
+  categoriesRef.once('value').then((snapshot) => {
+    const categories = snapshot.val();
+    const info = req.flash('info');
+    const hasInfo = info.length > 0;
+    res.render('dashboard/categories', { categories, info, hasInfo });
+  })
 });
 
 router.post('/categories/create', (req, res) => {
@@ -20,9 +28,26 @@ router.post('/categories/create', (req, res) => {
   const key = categoryRef.key;
   data.id = key;
 
-  categoryRef.set(data).then(() => {
+  categoriesRef.orderByChild('path').equalTo(data.path).once('value').then((snapshot) => {
+    if (snapshot.val() !== null) {
+      req.flash('info', `已有「${data.path}」路徑`);
+      res.redirect('/dashboard/categories');
+    } else {
+      categoryRef.set(data).then(() => {
+        res.redirect('/dashboard/categories');
+      });
+    }
+  });
+});
+
+router.post('/categories/delete/:id', (req, res) => {
+  const id = req.params.id;
+  categoriesRef.once('value').then((snapshot) => {
+    const category = snapshot.val()[id].name;
+    categoriesRef.child(id).remove();
+    req.flash('info', `「${category}」欄位已刪除`);
     res.redirect('/dashboard/categories');
-  })
-})
+  });
+});
 
 module.exports = router;
