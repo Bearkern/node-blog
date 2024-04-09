b144c2bf545684bfd171844ec77ee104666048e2
Clara Cai <clara.cai@tpisoftware.com>
2024-04-09 15:14:16 +0800

    add post page

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,44 @@
 const express = require('express');
 const router = express.Router();
+const firebaseAdminDb = require('../connections/firebase-admin');
+const striptags = require('striptags');
+const moment = require('moment');
+
+const categoriesRef = firebaseAdminDb.ref('categories');
+const articlesRef = firebaseAdminDb.ref('articles');
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  res.render('index', { title: 'Express' });
+  const status = 'public';
+  let categories = {};
+
+  categoriesRef.once('value').then((snapshot) => {
+    categories = snapshot.val();
+    return articlesRef.orderByChild('update_time').once('value');
+  }).then((snapshot) => {
+    const articles = [];
+    snapshot.forEach((snapshotChild) => {
+      if(status === snapshotChild.val().status) {
+        articles.push(snapshotChild.val());
+      }
+    });
+    articles.reverse();
+    res.render('index', { categories, articles, striptags, moment });
+  });
 });
 
-router.get('/post', (req, res, next) => {
-  res.render('post', { title: 'Express' });
+router.get('/post/:id', (req, res, next) => {
+  const id = req.params.id;
+  let categories = {};
+  categoriesRef.once('value').then((snapshot) => {
+    categories = snapshot.val();
+
+    return articlesRef.child(id).once('value');
+  }).then((snapshot) => {
+    const article = snapshot.val();
+
+    res.render('post', { categories, article, striptags, moment });
+  });
 });
 
 router.get('/dashboard/signup', (req, res, next) => {
